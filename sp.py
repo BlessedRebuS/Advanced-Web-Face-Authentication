e1436dc552ccd28bc203b51007c968d7eef8e27b
BlessedRebuS <patrick.difa@gmail.com>
2022-12-11 01:53:45 +0100

    aggiunto sp python e idp node

--- sp.py
+++ sp.py
@@ -1,74 +1,79 @@
-#!/usr/bin/env python3
-from flask import Flask, url_for
-
-from flask_saml2.sp import ServiceProvider
-from tests.idp.base import CERTIFICATE as IDP_CERTIFICATE
-from tests.sp.base import CERTIFICATE, PRIVATE_KEY
-
-
-class ExampleServiceProvider(ServiceProvider):
-    def get_logout_return_url(self):
-        return url_for('index', _external=True)
-
-    def get_default_login_return_url(self):
-        return url_for('index', _external=True)
-
-
-sp = ExampleServiceProvider()
+from flask import Flask, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+import requests
 
 app = Flask(__name__)
-app.debug = True
-app.secret_key = 'not a secret'
+app.secret_key = 'asdasdasd'  # Change this!
 
-app.config['SERVER_NAME'] = 'localhost:9000'
-app.config['SAML2_SP'] = {
-    'certificate': CERTIFICATE,
-    'private_key': PRIVATE_KEY,
-}
+# Setup Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-app.config['SAML2_IDENTITY_PROVIDERS'] = [
-    {
-        'CLASS': 'flask_saml2.sp.idphandler.IdPHandler',
-        'OPTIONS': {
-            'display_name': 'My Identity Provider',
-            'entity_id': 'http://localhost:8000/saml/metadata.xml',
-            'sso_url': 'http://localhost:8000/saml/login/',
-            'slo_url': 'http://localhost:8000/saml/logout/',
-            'certificate': IDP_CERTIFICATE,
-        },
-    },
-]
+# The base URL of the identity provider
+IDP_BASE_URL = 'http://localhost:3000'
 
+    
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+    
+    def __repr__(self):
+        return f"User: {self.id}"
 
-@app.route('/')
+def identify(username, password):
+  # Send a request to the identity provider to check the user's credentials
+  r = requests.get(
+    f'{IDP_BASE_URL}/profile',
+    headers={
+      'username': username,
+      'password': password
+    }
+  )
+  return r
+
+@login_manager.user_loader
+def user_loader(user_id):
+    user = User(user_id)
+    return user
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Get the username and password values from the form
+        username = request.form["username"]
+        password = request.form["password"]
+
+        r = identify(username, password)
+        if r.status_code == 200:
+            u = User(r.json())
+            login_user(u)
+            print(r.json())
+            return "User logged in"
+
+        else:
+            return 'Unauthorized: Invalid credentials', 401
+
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    # Otherwise, show the login page
+    return """
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+    """
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return "User logged out"
+
+@app.route("/")
+@login_required
 def index():
-    if sp.is_user_logged_in():
-        auth_data = sp.get_auth_data_in_session()
+    return "Home page"
 
-        message = f'''
-        <p>You are logged in as <strong>{auth_data.nameid}</strong>.
-        The IdP sent back the following attributes:<p>
-        '''
-
-        attrs = '<dl>{}</dl>'.format(''.join(
-            f'<dt>{attr}</dt><dd>{value}</dd>'
-            for attr, value in auth_data.attributes.items()))
-
-        logout_url = url_for('flask_saml2_sp.logout')
-        logout = f'<form action="{logout_url}" method="POST"><input type="submit" value="Log out"></form>'
-
-        return message + attrs + logout
-    else:
-        message = '<p>You are logged out.</p>'
-
-        login_url = url_for('flask_saml2_sp.login')
-        link = f'<p><a href="{login_url}">Log in to continue</a></p>'
-
-        return message + link
-
-
-app.register_blueprint(sp.create_blueprint(), url_prefix='/saml/')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+if __name__ == "__main__":
+    app.run()
